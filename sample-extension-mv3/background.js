aab5efc314ea8291c3f3c41056dd318168b1a13e
gfreeman30 <glendafreeman@zohomail.com>
2025-01-18 09:27:56 -0500

    Update background.js

--- sample-extension-mv3/background.js
+++ sample-extension-mv3/background.js
@@ -4,7 +4,7 @@ importScripts('ExtPay.js')
 // the extension you registered on ExtensionPay.com. You may
 // need to uninstall and reinstall the extension.
 // And don't forget to change the ID in popup.js too!
-var extpay = ExtPay('sample-extension'); 
+const extpay = ExtPay('quick-notes')
 extpay.startBackground(); // this line is required to use ExtPay in the rest of your extension
 
 extpay.getUser().then(user => {
